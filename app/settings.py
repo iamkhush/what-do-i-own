a4d5ef08a4dd9009a4e9bf54137d72fa0d4030e1
Ankush <ankush.chadda@gmail.com>
2025-01-25 22:25:10 +0100

    add allowed hosts to env

--- app/settings.py
+++ app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1'] + env.list('EXTRA_ALLOWED_HOSTS')
 CSRF_TRUSTED_ORIGINS = []
 
 

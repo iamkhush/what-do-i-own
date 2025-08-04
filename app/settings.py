3d0ff330298b3baffbab4c912a6e0c1888c01603
Ankush <ankush.chadda@gmail.com>
2025-08-04 02:11:49 +0200

    migrate from scripts to ansible

--- app/settings.py
+++ app/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = env("SERVER_PATH") + "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = env("STATIC_ROOT")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
